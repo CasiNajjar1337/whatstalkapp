b61294f9634e15203f03a0abfd48bae6bcc240a9
Meliksah <melikchuck@gmail.com>
2018-12-18 18:13:13 +0300

    updates

--- app/src/main/java/com/example/berat/whatstalk/HomeActivity.java
+++ app/src/main/java/com/example/berat/whatstalk/HomeActivity.java
@@ -1,11 +1,15 @@
 package com.example.berat.whatstalk;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,10 +21,14 @@ public class HomeActivity extends AppCompatActivity {
 
     private List<ListItem> listItems;
 
+    private FloatingActionButton fab;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fab = (FloatingActionButton)findViewById(R.id.floating_action_button);
 
         recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
@@ -42,12 +50,37 @@ public class HomeActivity extends AppCompatActivity {
 
         adapter = new MyAdapter(listItems, this);
         recyclerView.setAdapter(adapter);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, AddNumberActivity.class));
+
+            }
+        });
     }
 
-    public void addNumber(View view) {
-        Intent intent = new Intent(this, AddNumberActivity.class);
-        startActivity(intent);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
 
+        if(id==R.id.action_settings){
+            Toast.makeText(this,"Settings Tıklandı", Toast.LENGTH_SHORT).show();
+        }
+        if (id==R.id.action_privacy){
+            startActivity(new Intent(HomeActivity.this, AddNumberActivity.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //    public void addNumber(View view) {
+//        Intent intent = new Intent(this, AddNumberActivity.class);
+//        startActivity(intent);
 }
+
